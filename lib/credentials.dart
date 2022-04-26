4cfcfaced7da6adca4db6ca7585290f0054d0974
Globy <ludovic77240@gmail.com>
2022-04-26 13:28:56 +0200

    add readme

---
+++ lib/credentials.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
+const String CLIENT_UID = 'your_client_uid';
+const String CLIENT_SECRET = 'your_client_secret';
+const String GET_CODE_URL = "your_oauth_url";
